95e15469b42e36e5bdf803a02ac25f5fcb641dde
Geoffrey Yu <geoffxy@gmail.com>
2014-11-07 19:42:50 -0500

    Support strict and non-strict errors

--- test.js
+++ test.js
@@ -3,24 +3,21 @@
 var fs = require('fs');
 var colors = require('colors');
 var exec = require('child_process').exec;
+var yaml = require('js-yaml');
 
-// ./test.js [--strict] [root [compile command]]
-//
-// e.g.
-// ./test.js --strict
-// ./test.js ./ "g++ lexer.cc"
+// ./test.js [--nocompile] [root]
 
-var strict = process.argv[2] === '--strict';
+var noCompile = process.argv[2] === '--nocompile';
 var root;
 var compile;
 
-if (!strict) {
-  root = process.argv[2] || '.';
-  compile = process.argv[3] && process.argv[3].replace('"', '') || 'g++ ' + root + '/*.cc';
-} else {
+if (noCompile) {
   root = process.argv[3] || '.';
-  compile = process.argv[4] && process.argv[4].replace('"', '') || 'g++ ' + root + '/*.cc';
+} else {
+  root = process.argv[2] || '.';
 }
+
+compile = 'g++ ' + root + '/*.cc';
 
 var tests = root + '/tests';
 var testsIn = tests + '/in';
@@ -30,20 +27,32 @@ console.log();
 console.log('CS 241 Test Runner'.bold.underline);
 console.log('Running out of: ' + root);
 outputBreak();
-console.log('Compiling files using: ' + compile);
 
-exec(compile, function(err, stdout, stderr) {
-  var errorMsg = stderr && stderr.toString();
+if (!noCompile) {
+  console.log('Compiling files using: ' + compile);
 
-  if (err || errorMsg) {
-    console.log('Compilation error.'.red);
-    console.log(errorMsg);
-  } else {
-    console.log('Compilation succeeded.'.green);
-    outputBreak();
-    runTests();
-  }
-});
+  exec(compile, function(err, stdout, stderr) {
+    var errorMsg = stderr && stderr.toString();
+
+    if (err || errorMsg) {
+      console.log('Compilation error.'.red);
+      console.log(errorMsg);
+    } else {
+      console.log('Compilation succeeded.'.green);
+      outputBreak();
+      runTests();
+    }
+  });
+} else {
+  console.log('Skipping compilation.'.green);
+  console.log('Testing using: '.bold + root + '/a.out');
+  outputBreak();
+  runTests();
+}
+
+function removeExecutable() {
+  exec('rm a.out', function() {});
+}
 
 function runTests() {
   var testStart = Date.now();
@@ -52,7 +61,6 @@ function runTests() {
   var numCorrect = 0;
 
   console.log('Found ' + (allTests.length + "").bold + ' test(s).');
-  console.log(allTests);
 
   function complete() {
     if (numComplete === allTests.length) {
@@ -62,22 +70,46 @@ function runTests() {
       console.log('Summary: '.bold + numCorrect + '/' + numComplete);
       console.log('Test Suite Runtime: ' + ((Date.now() - testStart) / 1000) + 's');
       console.log();
+      !noCompile && removeExecutable();
+    }
+  }
+
+  function resolveTestCase(condition, testCase) {
+    if (condition) {
+      numCorrect++;
+      console.log('Pass - '.green + testCase);
+    } else {
+      console.log('Fail - '.red + testCase);
     }
   }
 
   allTests.forEach(function(testCase) {
-    var expectedOutputFile = testsOut + '/' + testCase.substr(0, testCase.lastIndexOf('.')) + '.out';
-    var expectedOutput = fs.readFileSync(expectedOutputFile, {encoding: 'utf8'});
+    var expectedOutputFile = testsOut + '/' + testCase.substr(0, testCase.lastIndexOf('.')) + '.yaml';
+    var expected = yaml.safeLoad(fs.readFileSync(expectedOutputFile, {encoding: 'utf8'}));
+    var executablePath = noCompile && root + '/a.out' || './a.out';
 
-    exec('./a.out < ' + testsIn + '/' + testCase, function(err, stdout, stderr) {
+    exec(executablePath + ' < ' + testsIn + '/' + testCase, function(err, stdout, stderr) {
       numComplete++;
       if (!err) {
-        if (stdout.toString() === expectedOutput) {
-          numCorrect++;
-          console.log('Pass - '.green + testCase);
+
+        if (!expected.error) {
+          // Successful run expected
+          resolveTestCase(stdout.toString() === expected.result, testCase);
         } else {
-          console.log('Fail - '.red + testCase);
+          // Error expected
+          var output = stderr.toString();
+
+          if (expected.strict) {
+            // Strict Error - Error message must match what is expected
+            resolveTestCase(output === expected.result, testCase);
+          } else {
+            // Non-strict Error - Error message only has to contain the result at some point
+            resolveTestCase(output.indexOf(expected.result) !== -1, testCase);
+          }
         }
+
+      } else {
+        console.log('ERROR: Executable not found.'.red);
       }
       complete();
     });
